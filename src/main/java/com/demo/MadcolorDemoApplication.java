c31d217b0eef50dd4bd22d4e9ca8abde1a5e0233
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-02-27 08:16:48 +0700

    init

---
+++ src/main/java/com/demo/MadcolorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadcolorDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadcolorDemoApplication.class, args);
+    }
+
+}
