6f771d25fabf1604c66a324fd9c19b0bceb73bc9
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-02-27 10:36:51 +0700

    SecurityConfiguration.java: configured

--- src/main/java/com/demo/config/SecurityConfiguration.java
+++ src/main/java/com/demo/config/SecurityConfiguration.java
@@ -1,4 +1,39 @@
 package com.demo.config;
 
+import com.demo.security.jwt.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
 public class SecurityConfiguration {
+    private static final String[] AUTH_WHITELIST = {
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/error/**",
+            "/api/authenticate"
+    };
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests()
+                .mvcMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+                .and().build();
+    }
 }
