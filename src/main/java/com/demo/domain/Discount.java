c8258c6b77eb120967beef9f96e07347d38d72f2
Nguyễn Hồng Hiệp <syaorancode@gmail.com>
2023-03-01 12:50:16 +0700

    done add discount

--- src/main/java/com/demo/domain/Discount.java
+++ src/main/java/com/demo/domain/Discount.java
@@ -17,10 +17,11 @@ public class Discount implements Serializable {
     private static final Long serialVersionUID = 1L;
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, unique = true)
+    @Column(length = 50, nullable = false, unique = true)
     private String name;
     @Column(nullable = false)
     private Integer percent;
+    @Column(length = 200)
     private String description;
     @Column(nullable = false)
     private Instant startAt;
---
+++ src/main/java/com/demo/web/api/DiscountResource.java
@@ -0,0 +1,21 @@
+package com.demo.web.api;
+
+import com.demo.domain.Discount;
+import com.demo.web.vm.DiscountVM;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@RequestMapping("/api")
+public interface DiscountResource {
+    @Operation(
+            security = @SecurityRequirement(name = "token_auth")
+    )
+    @PostMapping("/discount")
+    ResponseEntity<Discount> createDiscount(@RequestBody @Valid DiscountVM discountVM);
+}
